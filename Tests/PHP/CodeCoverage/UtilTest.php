94be6d97d68385712db509f58ecd057b087eb656
Sebastian Bergmann <sb@sebastian-bergmann.de>
2010-01-20 09:15:23 +0100

    Add test for PHP_CodeCoverage_Util::getPercent().

--- Tests/PHP/CodeCoverage/UtilTest.php
+++ Tests/PHP/CodeCoverage/UtilTest.php
@@ -481,6 +481,18 @@ class PHP_CodeCoverage_UtilTest extends PHPUnit_Framework_TestCase
         );
     }
 
+    /**
+     * @covers PHP_CodeCoverage_Util::percent
+     */
+    public function testPercent()
+    {
+        $this->assertEquals(100, PHP_CodeCoverage_Util::percent(100, 0));
+        $this->assertEquals(100, PHP_CodeCoverage_Util::percent(100, 100));
+        $this->assertEquals(
+          '100.00', PHP_CodeCoverage_Util::percent(100, 100, TRUE)
+        );
+    }
+
     public function getLinesToBeCoveredProvider()
     {
         return array(

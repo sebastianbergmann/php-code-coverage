c9dabbb07d6f79493b22f447b7bbf23fb99ebb49
Sebastian Bergmann <sb@sebastian-bergmann.de>
2011-08-28 12:32:57 +0200

    Include autoload script in list of files.

--- PHP/CodeCoverage/Autoload.php
+++ PHP/CodeCoverage/Autoload.php
@@ -78,7 +78,7 @@ function php_codecoverage_autoload($class = NULL) {
     }
 
     if ($class === NULL) {
-        $result = array();
+        $result = array(__FILE__);
 
         foreach ($classes as $file) {
             $result[] = $path . $file;

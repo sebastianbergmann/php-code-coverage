8d93720d5674688c928b501c778344f97764e7b3
Sebastian Bergmann <sb@sebastian-bergmann.de>
2012-01-13 07:59:04 +0100

    Update tests.

--- Tests/PHP/CodeCoverage/UtilTest.php
+++ Tests/PHP/CodeCoverage/UtilTest.php
@@ -196,15 +196,18 @@ class PHP_CodeCoverage_UtilTest extends PHPUnit_Framework_TestCase
     {
         $this->assertEquals(
           array(
+             1 => TRUE,
              3 => TRUE,
              4 => TRUE,
              5 => TRUE,
+             7 => TRUE,
             11 => TRUE,
             12 => TRUE,
             13 => TRUE,
             14 => TRUE,
             15 => TRUE,
             16 => TRUE,
+            17 => TRUE,
             18 => TRUE,
             19 => TRUE,
             20 => TRUE,
@@ -213,6 +216,8 @@ class PHP_CodeCoverage_UtilTest extends PHPUnit_Framework_TestCase
             23 => TRUE,
             24 => TRUE,
             25 => TRUE,
+            26 => TRUE,
+            27 => TRUE,
             30 => TRUE
           ),
           PHP_CodeCoverage_Util::getLinesToBeIgnored(
@@ -227,7 +232,7 @@ class PHP_CodeCoverage_UtilTest extends PHPUnit_Framework_TestCase
     public function testGetLinesToBeIgnored2()
     {
         $this->assertEquals(
-          array(),
+          array(1 => TRUE),
           PHP_CodeCoverage_Util::getLinesToBeIgnored(
             TEST_FILES_PATH . 'source_without_ignore.php'
           )

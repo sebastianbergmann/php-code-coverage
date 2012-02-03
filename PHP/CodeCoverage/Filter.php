ffda511c185b04762e81c6bd2e596f73b855f005
Sebastian Bergmann <sb@sebastian-bergmann.de>
2012-02-03 18:37:18 +0100

    Leftover.

--- PHP/CodeCoverage/Filter.php
+++ PHP/CodeCoverage/Filter.php
@@ -78,7 +78,6 @@ class PHP_CodeCoverage_Filter
     public function __construct()
     {
         $functions = array(
-          'file_iterator_autoload',
           'php_codecoverage_autoload',
           'php_invoker_autoload',
           'php_timer_autoload',
@@ -97,20 +96,24 @@ class PHP_CodeCoverage_Filter
             }
         }
 
-        $file = stream_resolve_include_path(
-          'SymfonyComponents/YAML/sfYaml.php'
-        );
-
-        if ($file) {
-            $this->addFileToBlacklist($file);
-        }
-
-        $file = stream_resolve_include_path(
+        $files = array(
+          'Symfony/Component/Finder/Finder.php',
+          'Symfony/Component/Finder/Glob.php',
+          'Symfony/Component/Finder/Iterator/FileTypeFilterIterator.php',
+          'Symfony/Component/Finder/Iterator/FilenameFilterIterator.php',
+          'Symfony/Component/Finder/Iterator/RecursiveDirectoryIterator.php',
+          'Symfony/Component/Finder/Iterator/ExcludeDirectoryFilterIterator.php',
+          'Symfony/Component/Finder/SplFileInfo.php',
+          'SymfonyComponents/YAML/sfYaml.php',
           'SymfonyComponents/YAML/sfYamlDumper.php'
         );
 
-        if ($file) {
-            $this->addFileToBlacklist($file);
+        foreach ($files as $file) {
+            $file = stream_resolve_include_path($file);
+
+            if ($file) {
+                $this->addFileToBlacklist($file);
+            }
         }
     }
 

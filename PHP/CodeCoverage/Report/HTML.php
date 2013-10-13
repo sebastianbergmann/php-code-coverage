774e7436e588e97df00836e48cc079bb1e43d47e
Tom Bevers <t.bevers@gmail.com>
2013-10-13 09:45:47 +0200

    Add NVD3.js and D3.js dependencies

--- PHP/CodeCoverage/Report/HTML.php
+++ PHP/CodeCoverage/Report/HTML.php
@@ -188,6 +188,7 @@ class PHP_CodeCoverage_Report_HTML
     {
         $dir = $this->getDirectory($target . 'css');
         copy($this->templatePath . 'css/bootstrap.min.css', $dir . 'bootstrap.min.css');
+        copy($this->templatePath . 'css/nv.d3.css', $dir . 'nv.d3.css');
         copy($this->templatePath . 'css/style.css', $dir . 'style.css');
 
         $dir = $this->getDirectory($target . 'fonts');
@@ -199,6 +200,8 @@ class PHP_CodeCoverage_Report_HTML
         $dir = $this->getDirectory($target . 'js');
         copy($this->templatePath . 'js/bootstrap.min.js', $dir . 'bootstrap.min.js');
         copy($this->templatePath . 'js/highcharts.js', $dir . 'highcharts.js');
+        copy($this->templatePath . 'js/d3.min.js', $dir . 'd3.min.js');
+        copy($this->templatePath . 'js/nv.d3.min.js', $dir . 'nv.d3.min.js');
         copy($this->templatePath . 'js/holder.js', $dir . 'holder.js');
         copy($this->templatePath . 'js/html5shiv.js', $dir . 'html5shiv.js');
         copy($this->templatePath . 'js/jquery.js', $dir . 'jquery.js');

35589033c8620d1fac89bf62c582f15eb2fa380f
Sebastian Bergmann <sb@sebastian-bergmann.de>
2014-03-17 11:21:13 +0100

    Fix blacklist entry for SebastianBergmann\Diff. Closes #226.

--- src/CodeCoverage/Filter.php
+++ src/CodeCoverage/Filter.php
@@ -312,7 +312,7 @@ class PHP_CodeCoverage_Filter
         $this->addDirectoryContainingClassToBlacklist('PHPUnit_Extensions_Story_TestCase', 2);
         $this->addDirectoryContainingClassToBlacklist('Text_Template');
         $this->addDirectoryContainingClassToBlacklist('Symfony\Component\Yaml\Yaml');
-        $this->addDirectoryContainingClassToBlacklist('SebastianBergmann\Diff');
+        $this->addDirectoryContainingClassToBlacklist('SebastianBergmann\Diff\Diff');
         $this->addDirectoryContainingClassToBlacklist('SebastianBergmann\Environment\Runtime');
         $this->addDirectoryContainingClassToBlacklist('SebastianBergmann\Exporter\Exporter');
         $this->addDirectoryContainingClassToBlacklist('SebastianBergmann\Version');

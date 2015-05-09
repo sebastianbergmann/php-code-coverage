9a5fd3c39541dfcb1761d1bbdf5074bae45837cc
Graham Campbell <graham@mineuk.com>
2015-05-09 10:29:41 +0100

    Updated holder.js to 2.7.1

--- src/CodeCoverage/Report/HTML.php
+++ src/CodeCoverage/Report/HTML.php
@@ -151,7 +151,7 @@ class PHP_CodeCoverage_Report_HTML
         $dir = $this->getDirectory($target . 'js');
         copy($this->templatePath . 'js/bootstrap.min.js', $dir . 'bootstrap.min.js');
         copy($this->templatePath . 'js/d3.min.js', $dir . 'd3.min.js');
-        copy($this->templatePath . 'js/holder.js', $dir . 'holder.js');
+        copy($this->templatePath . 'js/holder.min.js', $dir . 'holder.min.js');
         copy($this->templatePath . 'js/html5shiv.min.js', $dir . 'html5shiv.min.js');
         copy($this->templatePath . 'js/jquery.min.js', $dir . 'jquery.min.js');
         copy($this->templatePath . 'js/nv.d3.min.js', $dir . 'nv.d3.min.js');

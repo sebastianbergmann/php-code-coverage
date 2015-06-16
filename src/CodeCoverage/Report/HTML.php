684e2fea23dd6ae0b447e6b8c65266db3748b7a2
Stéphane <demonchaux.stephane@gmail.com>
2015-06-16 14:51:37 +0200

    Add list.min.js to copy directory

--- src/CodeCoverage/Report/HTML.php
+++ src/CodeCoverage/Report/HTML.php
@@ -156,6 +156,7 @@ class PHP_CodeCoverage_Report_HTML
         copy($this->templatePath . 'js/jquery.min.js', $dir . 'jquery.min.js');
         copy($this->templatePath . 'js/nv.d3.min.js', $dir . 'nv.d3.min.js');
         copy($this->templatePath . 'js/respond.min.js', $dir . 'respond.min.js');
+        copy($this->templatePath . 'js/list.min.js', $dir . 'list.min.js');
     }
 
     /**

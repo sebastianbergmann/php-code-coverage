ca72afdde86993a56a4b0bb21ed57e7b0f72ca30
Sebastian Bergmann <sb@sebastian-bergmann.de>
2017-01-02 09:00:31 +0100

    Closes https://github.com/sebastianbergmann/phpunit/issues/2410

--- src/CodeCoverage.php
+++ src/CodeCoverage.php
@@ -10,6 +10,8 @@
 
 namespace SebastianBergmann\CodeCoverage;
 
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Runner\PhptTestCase;
 use SebastianBergmann\CodeCoverage\Driver\Driver;
 use SebastianBergmann\CodeCoverage\Driver\Xdebug;
 use SebastianBergmann\CodeCoverage\Driver\HHVM;
@@ -340,7 +342,7 @@ class CodeCoverage
         $size   = 'unknown';
         $status = null;
 
-        if ($id instanceof \PHPUnit_Framework_TestCase) {
+        if ($id instanceof TestCase) {
             $_size = $id->getSize();
 
             if ($_size == \PHPUnit\Util\Test::SMALL) {
@@ -353,7 +355,7 @@ class CodeCoverage
 
             $status = $id->getStatus();
             $id     = get_class($id) . '::' . $id->getName();
-        } elseif ($id instanceof \PHPUnit_Extensions_PhptTestCase) {
+        } elseif ($id instanceof PhptTestCase) {
             $size = 'large';
             $id   = $id->getName();
         }
@@ -619,7 +621,7 @@ class CodeCoverage
         }
 
         if ($this->checkForUnintentionallyCoveredCode &&
-            (!$this->currentId instanceof \PHPUnit_Framework_TestCase ||
+            (!$this->currentId instanceof TestCase ||
             (!$this->currentId->isMedium() && !$this->currentId->isLarge()))) {
             $this->performUnintentionallyCoveredCodeCheck(
                 $data,

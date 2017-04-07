87929fd5b42c19c7ee243b58bd2107182eaa3c11
Arne Blankerts <Arne@Blankerts.de>
2017-04-07 16:27:48 +0200

    Add whitespace

--- src/Report/Xml/Source.php
+++ src/Report/Xml/Source.php
@@ -7,6 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace SebastianBergmann\CodeCoverage\Report\Xml;
 
 use TheSeer\Tokenizer\NamespaceUri;

a5ba7cb3cdc41dee65b5607a38517cccfdf8522e
bugreportuser <37939393+bugreportuser@users.noreply.github.com>
2018-10-11 13:25:18 -0600

    Fix file_exists() on non-file

--- src/Driver/Xdebug.php
+++ src/Driver/Xdebug.php
@@ -66,7 +66,7 @@ final class Xdebug implements Driver
         foreach (\array_keys($data) as $file) {
             unset($data[$file][0]);
 
-            if (\strpos($file, 'xdebug://debug-eval') !== 0 && \file_exists($file)) {
+            if (\strpos($file, 'xdebug://debug-eval') !== 0 && $file !== 'Standard input code' && \file_exists($file)) {
                 $numLines = $this->getNumberOfLinesInFile($file);
 
                 foreach (\array_keys($data[$file]) as $line) {

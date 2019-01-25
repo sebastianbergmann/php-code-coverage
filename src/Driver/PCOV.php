5767e3f2d5c0c5caac06428131eecfb3c84ec801
Joe Watkins <krakjoe@php.net>
2019-01-25 07:31:45 +0100

    Driver

---
+++ src/Driver/PCOV.php
@@ -0,0 +1,58 @@
+<?php
+/*
+ * This file is part of the php-code-coverage package.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace SebastianBergmann\CodeCoverage\Driver;
+
+use SebastianBergmann\CodeCoverage\RuntimeException;
+use SebastianBergmann\CodeCoverage\Filter;
+
+/**
+ * Driver for PCOV code coverage functionality.
+ *
+ * @codeCoverageIgnore
+ */
+final class PCOV implements Driver
+{
+    /**
+     * @throws RuntimeException
+     */
+    public function __construct(Filter $filter = null)
+    {
+	
+    }
+
+    /**
+     * Start collection of code coverage information.
+     */
+    public function start(bool $determineUnusedAndDead = true): void
+    {
+        \pcov\start();
+    }
+
+    /**
+     * Stop collection of code coverage information.
+     */
+    public function stop(): array
+    {
+	\pcov\stop();
+
+	$waiting = \pcov\waiting();
+	$collect  = [];
+
+	if ($waiting) {
+		$collect = \pcov\collect(\pcov\inclusive, $waiting);
+
+		if ($collect) {
+			\pcov\clear();
+		}
+	}
+
+	return $collect;
+    }
+}

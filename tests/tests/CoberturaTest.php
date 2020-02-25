94b999b5cf06e9fc1bd550ab33386c822e90814e
Otto Rask <ojrask@gmail.com>
2020-02-25 02:06:33 +0200

    Add initial failing tests for new Cobertura report format

---
+++ tests/tests/CoberturaTest.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types = 1);
+/*
+ * This file is part of phpunit/php-code-coverage.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace SebastianBergmann\CodeCoverage\Report;
+
+use SebastianBergmann\CodeCoverage\CodeCoverage;
+use SebastianBergmann\CodeCoverage\TestCase;
+
+/**
+ * @covers \SebastianBergmann\CodeCoverage\Report\Cobertura
+ */
+class CoberturaTest extends TestCase
+{
+    public function testCloverForBankAccountTest(): void
+    {
+        $cobertura = new Cobertura;
+
+        $this->assertStringMatchesFormatFile(
+            TEST_FILES_PATH . 'BankAccount-cobertura.xml',
+            $cobertura->process($this->getCoverageForBankAccount(), 'BankAccount')
+        );
+    }
+
+    public function testCloverForFileWithIgnoredLines(): void
+    {
+        $this->markTestIncomplete();
+
+        $cobertura = new Cobertura;
+
+        $this->assertStringMatchesFormatFile(
+            TEST_FILES_PATH . 'ignored-lines-clover.xml',
+            $cobertura->process($this->getCoverageForFileWithIgnoredLines(), 'BankAccount')
+        );
+    }
+
+    public function testCloverForClassWithAnonymousFunction(): void
+    {
+        $this->markTestIncomplete();
+
+        $cobertura = new Cobertura;
+
+        $this->assertStringMatchesFormatFile(
+            TEST_FILES_PATH . 'class-with-anonymous-function-clover.xml',
+            $cobertura->process($this->getCoverageForClassWithAnonymousFunction(), 'BankAccount')
+        );
+    }
+}

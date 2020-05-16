8b0341bab99e9c14300545d1286c1395390b36b4
Doug Wright <github@dougweb.org>
2020-05-16 13:43:18 +0100

    Store captured branch/path function data

--- src/RawCodeCoverageData.php
+++ src/RawCodeCoverageData.php
@@ -21,30 +21,43 @@ final class RawCodeCoverageData
      */
     private $lineCoverage = [];
 
+    /**
+     * @var array
+     *
+     * @see https://xdebug.org/docs/code_coverage for format
+     */
+    private $functionCoverage = [];
+
     public static function fromXdebugWithoutPathCoverage(array $rawCoverage): self
     {
-        return new self($rawCoverage);
+        return new self($rawCoverage, []);
     }
 
     public static function fromXdebugWithPathCoverage(array $rawCoverage): self
     {
-        $lineCoverage = [];
+        $lineCoverage = $functionCoverage = [];
 
         foreach ($rawCoverage as $file => $fileCoverageData) {
-            $lineCoverage[$file] = $fileCoverageData['lines'];
+            if (isset($fileCoverageData['functions'])) {
+                $lineCoverage[$file]     = $fileCoverageData['lines'];
+                $functionCoverage[$file] = $fileCoverageData['functions'];
+            } else { // not every file has functions, Xdebug outputs just line data for these
+                $lineCoverage[$file] = $fileCoverageData;
+            }
         }
 
-        return new self($lineCoverage);
+        return new self($lineCoverage, $functionCoverage);
     }
 
-    private function __construct(array $lineCoverage)
+    private function __construct(array $lineCoverage, array $functionCoverage)
     {
-        $this->lineCoverage = $lineCoverage;
+        $this->lineCoverage     = $lineCoverage;
+        $this->functionCoverage = $functionCoverage;
     }
 
     public function clear(): void
     {
-        $this->lineCoverage = [];
+        $this->lineCoverage = $this->functionCoverage = [];
     }
 
     public function getLineCoverage(): array
@@ -54,7 +67,7 @@ final class RawCodeCoverageData
 
     public function removeCoverageDataForFile(string $filename): void
     {
-        unset($this->lineCoverage[$filename]);
+        unset($this->lineCoverage[$filename], $this->functionCoverage[$filename]);
     }
 
     /**

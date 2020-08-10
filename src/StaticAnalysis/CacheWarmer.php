1360efde291f2dcb3f73eaaab433f52d9cd3b34f
Sebastian Bergmann <sb@sebastian-bergmann.de>
2020-08-10 09:18:55 +0200

    Fix CS/WS issues

--- src/StaticAnalysis/CacheWarmer.php
+++ src/StaticAnalysis/CacheWarmer.php
@@ -31,12 +31,16 @@ final class CacheWarmer
         foreach ($filter->files() as $file) {
             /* @noinspection UnusedFunctionResultInspection */
             $coveredFileAnalyser->classesIn($file);
+
             /* @noinspection UnusedFunctionResultInspection */
             $coveredFileAnalyser->traitsIn($file);
+
             /* @noinspection UnusedFunctionResultInspection */
             $coveredFileAnalyser->functionsIn($file);
+
             /* @noinspection UnusedFunctionResultInspection */
             $coveredFileAnalyser->linesOfCodeFor($file);
+
             /* @noinspection UnusedFunctionResultInspection */
             $coveredFileAnalyser->ignoredLinesFor($file);
 

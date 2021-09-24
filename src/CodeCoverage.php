8604ff365fb0f82902b2fec7f5be3eb5015ece8b
Doug Wright <github@dougweb.org>
2021-09-24 21:28:46 +0100

    #864 Uncovered files should be ignored unless requested

--- src/CodeCoverage.php
+++ src/CodeCoverage.php
@@ -167,6 +167,8 @@ final class CodeCoverage
                 $this->processUncoveredFilesFromFilter();
             } elseif ($this->includeUncoveredFiles) {
                 $this->addUncoveredFilesFromFilter();
+            } else {
+                $this->data->removeFilesWithNoCoverage();
             }
         }
 

2dc34be48f40c16c963f04beab2123abb380c70f
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-03-20 16:37:25 +0100

    Remove superfluous null checks

--- src/Report/Cobertura.php
+++ src/Report/Cobertura.php
@@ -174,7 +174,7 @@ final class Cobertura
                     $methodElement->appendChild($methodLinesElement);
 
                     foreach (range($method['startLine'], $method['endLine']) as $line) {
-                        if (!isset($coverageData[$line]) || $coverageData[$line] === null) {
+                        if (!isset($coverageData[$line])) {
                             continue;
                         }
                         $methodLineElement = $document->createElement('line');
@@ -255,7 +255,7 @@ final class Cobertura
                 $methodElement->appendChild($methodLinesElement);
 
                 foreach (range($function['startLine'], $function['endLine']) as $line) {
-                    if (!isset($coverageData[$line]) || $coverageData[$line] === null) {
+                    if (!isset($coverageData[$line])) {
                         continue;
                     }
                     $methodLineElement = $document->createElement('line');

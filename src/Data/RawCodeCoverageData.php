1cfad15609bb22b6c7908baa5eabaf729ab611c9
Doug Wright <github@dougweb.org>
2023-12-10 17:20:39 +0000

    Fixes #1023 Strip Xdebug annotations from function names in traits

--- src/Data/RawCodeCoverageData.php
+++ src/Data/RawCodeCoverageData.php
@@ -19,7 +19,10 @@ use function explode;
 use function file_get_contents;
 use function in_array;
 use function is_file;
+use function preg_replace;
 use function range;
+use function str_ends_with;
+use function str_starts_with;
 use function trim;
 use SebastianBergmann\CodeCoverage\Driver\Driver;
 use SebastianBergmann\CodeCoverage\StaticAnalysis\FileAnalyser;
@@ -65,6 +68,15 @@ final class RawCodeCoverageData
         $functionCoverage = [];
 
         foreach ($rawCoverage as $file => $fileCoverageData) {
+            // Xdebug annotates the function name of traits, strip that off
+            foreach ($fileCoverageData['functions'] as $existingKey => $data) {
+                if (str_ends_with($existingKey, '}') && !str_starts_with($existingKey, '{')) { // don't want to catch {main}
+                    $newKey                                 = preg_replace('/\{.*}$/', '', $existingKey);
+                    $fileCoverageData['functions'][$newKey] = $data;
+                    unset($fileCoverageData['functions'][$existingKey]);
+                }
+            }
+
             $lineCoverage[$file]     = $fileCoverageData['lines'];
             $functionCoverage[$file] = $fileCoverageData['functions'];
         }

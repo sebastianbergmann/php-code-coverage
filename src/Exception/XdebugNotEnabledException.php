467bb734857d88be798dcc54bf0b33adb71e7075
Sebastian Bergmann <sb@sebastian-bergmann.de>
2024-03-22 06:17:40 +0100

    Improve exception message

--- src/Exception/XdebugNotEnabledException.php
+++ src/Exception/XdebugNotEnabledException.php
@@ -16,6 +16,6 @@ final class XdebugNotEnabledException extends RuntimeException implements Except
 {
     public function __construct()
     {
-        parent::__construct('XDEBUG_MODE=coverage or xdebug.mode=coverage has to be set');
+        parent::__construct('XDEBUG_MODE=coverage (environment variable) or xdebug.mode=coverage (PHP configuration setting) has to be set');
     }
 }

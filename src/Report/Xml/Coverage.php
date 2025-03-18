beb5d44686b5fc33ad11a8aded8dfa5c3fbe90f4
Sebastian Bergmann <sb@sebastian-bergmann.de>
2025-03-18 09:00:13 +0100

    Fix method name

--- src/Report/Xml/Coverage.php
+++ src/Report/Xml/Coverage.php
@@ -28,7 +28,7 @@ final class Coverage
 
         $this->writer = new XMLWriter;
         $this->writer->openMemory();
-        $this->writer->startElementNS(null, $context->nodeName, 'https://schema.phpunit.de/coverage/1.0');
+        $this->writer->startElementNs(null, $context->nodeName, 'https://schema.phpunit.de/coverage/1.0');
         $this->writer->writeAttribute('nr', $line);
     }
 

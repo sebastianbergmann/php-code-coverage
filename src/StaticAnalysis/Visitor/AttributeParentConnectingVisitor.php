74cfe4eec2e9f8fcf98aa323255dd463dee24e15
Sebastian Bergmann <sb@sebastian-bergmann.de>
2025-05-02 07:44:16 +0200

    Fix CS/WS issues

--- src/StaticAnalysis/Visitor/AttributeParentConnectingVisitor.php
+++ src/StaticAnalysis/Visitor/AttributeParentConnectingVisitor.php
@@ -37,10 +37,8 @@ final class AttributeParentConnectingVisitor implements NodeVisitor
 
     public function enterNode(Node $node): null
     {
-        if (
-            $this->stack !== [] &&
-            ($node instanceof Node\Attribute || $node instanceof Node\AttributeGroup)
-        ) {
+        if ($this->stack !== [] &&
+            ($node instanceof Node\Attribute || $node instanceof Node\AttributeGroup)) {
             $node->setAttribute('parent', $this->stack[count($this->stack) - 1]);
         }
 

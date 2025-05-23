b212556a151aeb63ab5a10e0b513d8dbd1c1ca66
Sebastian Bergmann <sb@sebastian-bergmann.de>
2025-05-23 13:57:52 +0200

    Keep Report\Clover as-is and introduce new Report\OpenClover for reporter that generates Clover XML that validates against OpenClover's XML schema definition

--- tests/tests/Report/CloverTest.php
+++ tests/tests/Report/CloverTest.php
@@ -9,13 +9,6 @@
  */
 namespace SebastianBergmann\CodeCoverage\Report;
 
-use const PHP_EOL;
-use function libxml_clear_errors;
-use function libxml_get_errors;
-use function libxml_use_internal_errors;
-use function sprintf;
-use function trim;
-use DOMDocument;
 use PHPUnit\Framework\Attributes\CoversClass;
 use SebastianBergmann\CodeCoverage\TestCase;
 
@@ -26,8 +19,8 @@ final class CloverTest extends TestCase
     {
         $clover = new Clover;
 
-        $this->assertAndValidate(
-            TEST_FILES_PATH . 'BankAccount-clover-line.xml',
+        $this->assertStringMatchesFormatFile(
+            TEST_FILES_PATH . 'Report/Clover/BankAccount-line.xml',
             $clover->process($this->getLineCoverageForBankAccount(), null, 'BankAccount'),
         );
     }
@@ -36,8 +29,8 @@ final class CloverTest extends TestCase
     {
         $clover = new Clover;
 
-        $this->assertAndValidate(
-            TEST_FILES_PATH . 'BankAccount-clover-path.xml',
+        $this->assertStringMatchesFormatFile(
+            TEST_FILES_PATH . 'Report/Clover/BankAccount-path.xml',
             $clover->process($this->getPathCoverageForBankAccount(), null, 'BankAccount'),
         );
     }
@@ -46,8 +39,8 @@ final class CloverTest extends TestCase
     {
         $clover = new Clover;
 
-        $this->assertAndValidate(
-            TEST_FILES_PATH . 'ignored-lines-clover.xml',
+        $this->assertStringMatchesFormatFile(
+            TEST_FILES_PATH . 'Report/Clover/ignored-lines.xml',
             $clover->process($this->getCoverageForFileWithIgnoredLines()),
         );
     }
@@ -56,43 +49,9 @@ final class CloverTest extends TestCase
     {
         $clover = new Clover;
 
-        $this->assertAndValidate(
-            TEST_FILES_PATH . 'class-with-anonymous-function-clover.xml',
+        $this->assertStringMatchesFormatFile(
+            TEST_FILES_PATH . 'Report/Clover/class-with-anonymous-function.xml',
             $clover->process($this->getCoverageForClassWithAnonymousFunction()),
         );
-    }
-
-    /**
-     * @param non-empty-string $expectationFile
-     * @param non-empty-string $cloverXml
-     */
-    private function assertAndValidate(string $expectationFile, string $cloverXml): void
-    {
-        $this->assertStringMatchesFormatFile($expectationFile, $cloverXml);
-
-        libxml_use_internal_errors(true);
-
-        $document = new DOMDocument;
-        $document->loadXML($cloverXml);
-
-        if (!$document->schemaValidate(__DIR__ . '/../../_files/clover.xsd')) {
-            $buffer = 'Generated XML document does not validate against Clover schema:' . PHP_EOL . PHP_EOL;
-
-            foreach (libxml_get_errors() as $error) {
-                $buffer .= sprintf(
-                    '- Line %d: %s' . PHP_EOL,
-                    $error->line,
-                    trim($error->message),
-                );
-            }
-
-            $buffer .= PHP_EOL;
-        }
-
-        libxml_clear_errors();
-
-        if (isset($buffer)) {
-            $this->fail($buffer);
-        }
     }
 }
---
+++ tests/tests/Report/OpenCloverTest.php
@@ -0,0 +1,98 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of phpunit/php-code-coverage.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace SebastianBergmann\CodeCoverage\Report;
+
+use const PHP_EOL;
+use function libxml_clear_errors;
+use function libxml_get_errors;
+use function libxml_use_internal_errors;
+use function sprintf;
+use function trim;
+use DOMDocument;
+use PHPUnit\Framework\Attributes\CoversClass;
+use SebastianBergmann\CodeCoverage\TestCase;
+
+#[CoversClass(OpenClover::class)]
+final class OpenCloverTest extends TestCase
+{
+    public function testLineCoverageForBankAccountTest(): void
+    {
+        $clover = new OpenClover;
+
+        $this->assertAndValidate(
+            TEST_FILES_PATH . 'Report/OpenClover/BankAccount-line.xml',
+            $clover->process($this->getLineCoverageForBankAccount(), null, 'BankAccount'),
+        );
+    }
+
+    public function testPathCoverageForBankAccountTest(): void
+    {
+        $clover = new OpenClover;
+
+        $this->assertAndValidate(
+            TEST_FILES_PATH . 'Report/OpenClover/BankAccount-path.xml',
+            $clover->process($this->getPathCoverageForBankAccount(), null, 'BankAccount'),
+        );
+    }
+
+    public function testCloverForFileWithIgnoredLines(): void
+    {
+        $clover = new OpenClover;
+
+        $this->assertAndValidate(
+            TEST_FILES_PATH . 'Report/OpenClover/ignored-lines.xml',
+            $clover->process($this->getCoverageForFileWithIgnoredLines()),
+        );
+    }
+
+    public function testCloverForClassWithAnonymousFunction(): void
+    {
+        $clover = new OpenClover;
+
+        $this->assertAndValidate(
+            TEST_FILES_PATH . 'Report/OpenClover/class-with-anonymous-function.xml',
+            $clover->process($this->getCoverageForClassWithAnonymousFunction()),
+        );
+    }
+
+    /**
+     * @param non-empty-string $expectationFile
+     * @param non-empty-string $cloverXml
+     */
+    private function assertAndValidate(string $expectationFile, string $cloverXml): void
+    {
+        $this->assertStringMatchesFormatFile($expectationFile, $cloverXml);
+
+        libxml_use_internal_errors(true);
+
+        $document = new DOMDocument;
+        $document->loadXML($cloverXml);
+
+        if (!$document->schemaValidate(__DIR__ . '/../../_files/clover.xsd')) {
+            $buffer = 'Generated XML document does not validate against Clover schema:' . PHP_EOL . PHP_EOL;
+
+            foreach (libxml_get_errors() as $error) {
+                $buffer .= sprintf(
+                    '- Line %d: %s' . PHP_EOL,
+                    $error->line,
+                    trim($error->message),
+                );
+            }
+
+            $buffer .= PHP_EOL;
+        }
+
+        libxml_clear_errors();
+
+        if (isset($buffer)) {
+            $this->fail($buffer);
+        }
+    }
+}

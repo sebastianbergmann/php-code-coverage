da3b6149d7370499e24d5e1d3fc517515cb31223
Sebastian Bergmann <sb@sebastian-bergmann.de>
2025-09-11 07:57:08 +0200

    Use Xml::asString() and Filesystem::write()

--- src/Report/OpenClover.php
+++ src/Report/OpenClover.php
@@ -12,13 +12,10 @@ namespace SebastianBergmann\CodeCoverage\Report;
 use function assert;
 use function basename;
 use function count;
-use function dirname;
-use function file_put_contents;
 use function is_string;
 use function ksort;
 use function max;
 use function range;
-use function str_contains;
 use function str_replace;
 use function time;
 use DOMDocument;
@@ -26,6 +23,7 @@ use DOMElement;
 use SebastianBergmann\CodeCoverage\CodeCoverage;
 use SebastianBergmann\CodeCoverage\Node\File;
 use SebastianBergmann\CodeCoverage\Util\Filesystem;
+use SebastianBergmann\CodeCoverage\Util\Xml;
 use SebastianBergmann\CodeCoverage\Version;
 use SebastianBergmann\CodeCoverage\WriteOperationFailedException;
 
@@ -240,16 +238,10 @@ final class OpenClover
         $xmlMetrics->setAttribute('coveredmethods', (string) $report->numberOfTestedMethods());
         $xmlProject->insertBefore($xmlMetrics, $xmlProject->firstChild);
 
-        $buffer = $xmlDocument->saveXML();
+        $buffer = Xml::asString($xmlDocument);
 
         if ($target !== null) {
-            if (!str_contains($target, '://')) {
-                Filesystem::createDirectory(dirname($target));
-            }
-
-            if (@file_put_contents($target, $buffer) === false) {
-                throw new WriteOperationFailedException($target);
-            }
+            Filesystem::write($target, $buffer);
         }
 
         return $buffer;

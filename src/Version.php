bca180c050dd3ae15f87c26d25cabb34fe1a0a5a
Sebastian Bergmann <sb@sebastian-bergmann.de>
2025-11-29 08:15:54 +0100

    Prepare release

--- src/Version.php
+++ src/Version.php
@@ -19,7 +19,7 @@ final class Version
     public static function id(): string
     {
         if (self::$version === '') {
-            self::$version = (new VersionId('12.4.0', dirname(__DIR__)))->asString();
+            self::$version = (new VersionId('12.5.0', dirname(__DIR__)))->asString();
         }
 
         return self::$version;
